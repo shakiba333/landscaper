39129c7fd4898deb36a794e423723a49af4d10ad
shakiba333 <shakibaabdulrashid@gmail.com>
2023-07-06 14:08:03 -0400

    landscaper: win scenario

--- game.py
+++ game.py
@@ -1,13 +1,76 @@
 money = 0
-tools = []
 userChoice = ""
+tools = [
+    {
+        "tool": "teeth",
+        "income": 1 ,
+        "price": 0
+    },
+    {
+        "tool": "scissors",
+        "income": 5,
+        "price": 5
+    },
+    {
+        "tool": "lawnmower",
+        "income": 50,
+        "price": 25
+    },
+    {
+        "tool": "battery powered lawnmower",
+        "income": 100,
+        "price": 250
+    },
+    {
+        "tool": "hired students",
+        "income": 1000,
+        "price": 500
+    }
+]
 
-while userChoice != "quit":
-    userChoice = input("Using just your teeth, you can spend the day cutting lawns and make $1.\n")
-    if "teeth" in userChoice:
-        money = money + 1;
-        print(money)
-     
+my_inventory = [
+    {
+        "tool": "teeth",
+        "income": 1,
+        "price": 0
+    }
+]
+
+
+def check_tools():
+    global money
+    last_bought_tools = my_inventory[-1]
+    userChoice = input(f"You can spend the day cutting lawns and make ${last_bought_tools['income']}. Using just your {last_bought_tools['tool']}: y or n\n")
+    if 'y' in userChoice:
+        money = money + last_bought_tools['income'];
+        print(f"your income now equals to ${money}")
+        if money == 1000:
+            print("You've won!")
+    else:
+        return True    
+        
+
+
+
+def buy_tools():
+    global money
+    for tool in tools:
+        if  money == tool["price"]:
+            userChoice = input(f"You can buy {tool['tool']} for ${tool['price']}:y or n\n")
+            if 'y' in userChoice:
+                my_inventory.append(tool)
+                money = money - tool["price"]
+                check_tools()
+
+
+while userChoice != "n" and money < 1000:
+    if check_tools():
+        break
+    buy_tools()
+
+
+
+
 
 
 
